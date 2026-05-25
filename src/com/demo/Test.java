2b17e2b2fa727c21a8ed43896ac62c2370336a74
yashikashelkey <yashikashelkey@gmail.com>
2026-05-25 20:52:38 +0530

    My second commit

--- src/com/demo/Test.java
+++ src/com/demo/Test.java
@@ -3,5 +3,6 @@ package com.demo;
 public class Test {
 public static void main(String[] args) {
 	System.out.println("Welcome to Java world");
+	System.out.println("I am committed to learn Java");
 }
 }
